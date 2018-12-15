6d2d4894fa8117ad452b3809aa87a9ef13113ba0
Mathias Oertel <f7ff8e6f@opayq.com>
2018-12-15 10:18:33 +0100

    changed toolbars search box from using EditText to SearchWiew support v7

--- app/src/main/java/mort/ar/searxme/MainActivity.kt
+++ app/src/main/java/mort/ar/searxme/MainActivity.kt
@@ -3,13 +3,17 @@ package mort.ar.searxme
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.SearchView
+import android.support.v7.widget.Toolbar
 import android.text.Editable
 import android.text.TextUtils
 import android.text.TextWatcher
+import android.view.Menu
 import android.view.View
-import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.Toast
-import android.widget.Toolbar
 import dagger.android.AndroidInjection
 import io.reactivex.Observable
 import io.reactivex.ObservableEmitter
@@ -18,7 +22,6 @@ import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.plusAssign
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.toolbar.*
 import mort.ar.searxme.manager.SearchManager
 import mort.ar.searxme.model.SearxResult
 import javax.inject.Inject
@@ -51,9 +54,10 @@ class MainActivity : AppCompatActivity(),
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar as Toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
 
-        setActionBar(toolbar as Toolbar)
-        searchBoxToolbar.setOnEditorActionListener { _, actionId, _ ->
+        /*searchBoxToolbar.setOnEditorActionListener { _, actionId, _ ->
             when (actionId) {
                 EditorInfo.IME_ACTION_DONE -> {
                     if (!TextUtils.isEmpty(searchBoxToolbar.text.toString())) {
@@ -83,9 +87,60 @@ class MainActivity : AppCompatActivity(),
             .subscribeOn(Schedulers.io())
             .subscribe { isEmpty ->
                 searchBoxClearButton.visibility = if (isEmpty) View.GONE else View.VISIBLE
-            }
+            }*/
 
         replaceFragment(mActiveFragment)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+
+        val search = menu?.findItem(R.id.action_search)
+        val searchView = search?.actionView as SearchView
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                doSearch(query)
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+        })
+
+        setSearchViewStyle(searchView)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    /**
+     * Initiates the styling of toolbars search box
+     */
+    private fun setSearchViewStyle(searchView: SearchView) {
+        searchView.setIconifiedByDefault(false)
+        searchView.isIconified = false
+        searchView.isSubmitButtonEnabled = false
+        searchView.queryHint = getString(R.string.fragment_start_searchbox_hint)
+
+        val searchPlate = searchView.findViewById(android.support.v7.appcompat.R.id.search_plate) as View
+        searchPlate.setBackgroundColor(resources.getColor(R.color.colorPrimary))
+
+        val searchIcon = searchView.findViewById(android.support.v7.appcompat.R.id.search_mag_icon) as ImageView
+        searchIcon.setColorFilter(getColor(R.color.toolbar_icons))
+        searchIcon.layoutParams = LinearLayout.LayoutParams(0, 0)
+
+        val closeButton = searchView.findViewById(android.support.v7.appcompat.R.id.search_close_btn) as ImageView
+        closeButton.setColorFilter(getColor(R.color.toolbar_icons))
+
+        val submitButton = searchView.findViewById(android.support.v7.appcompat.R.id.search_go_btn) as ImageView
+        submitButton.setColorFilter(getColor(R.color.toolbar_icons))
+
+        val submitButtonArea = searchView.findViewById(android.support.v7.appcompat.R.id.submit_area) as View
+        submitButtonArea.setBackgroundColor(resources.getColor(R.color.colorPrimary))
+
+        val searchEditText = searchView.findViewById(android.support.v7.appcompat.R.id.search_src_text) as EditText
+        searchEditText.background = getDrawable(R.drawable.edittext_background)
     }
 
     private fun replaceFragment(fragment: Fragment) {
