41742d59c4b444fe77772fe861a273838e5e50a2
moertel <e0e9cf7e@opayq.com>
2019-08-09 12:45:50 +0200

    version updates

--- app/src/main/java/mort/ar/searxme/presentation/settings/SettingsPresenter.kt
+++ app/src/main/java/mort/ar/searxme/presentation/settings/SettingsPresenter.kt
@@ -6,9 +6,9 @@ import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.addTo
 import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.schedulers.Schedulers
+import mort.ar.searxme.data.model.SettingsParameter
 import mort.ar.searxme.domain.GetSettingsParameterUseCase
 import mort.ar.searxme.domain.SaveSettingsParameterUseCase
-import mort.ar.searxme.data.model.SettingsParameter
 import mort.ar.searxme.presentation.model.Categories
 import mort.ar.searxme.presentation.model.Engines
 import javax.inject.Inject
